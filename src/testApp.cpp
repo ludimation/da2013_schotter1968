e01a2fb8e3bfcf7ed249c32de6fcdc11c3225121
ludimation <mail@dallendesign.com>
2013-10-20 13:49:08 -0400

    Added drawingCount functionality so that pauses between drawings did not have to manipulate framerate, or use the system "Wait" function.

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -2,18 +2,24 @@
 
 //--------------------------------------------------------------
 void testApp::setup(){
+    
+    drawingCount    = -1;
+    waitMillis      = 5000;
+    waitMod         = 1024;
+    
 	ofBackground(ofColor::white);       // set bakcground color of piece to white
 	ofNoFill();                         // draw empty shapes
 	ofSetColor(ofColor::black);         // use black as stroke color
-	ofSetFrameRate(1);                 // slow down draw function a bit (default is 60fps?)
+	ofSetFrameRate(90);                 // not sure what the upper bound is, but yeah... shoot for the stars
 	ofSetRectMode(OF_RECTMODE_CENTER);  // set rect mode to draw rectangles from center of object
     
     // TO DO - add signature and citation of original work "###/° | david_allen_2013 ("Georg Nees - Schotter 1968")"
     ///////////////
 	//old OF default is 96 - but this results in fonts looking larger than in other programs. 
 	ofTrueTypeFont::setGlobalDpi(72);
-    ofSeedRandom(19800512);
-    ofResetElapsedTimeCounter();
+
+    // create the first drawing [0]
+    updateDrawing();
 }
 
 //--------------------------------------------------------------
@@ -24,6 +30,8 @@ void testApp::update(){
 //--------------------------------------------------------------
 void testApp::buildSquares(){
     squares.clear();
+    
+    ofSeedRandom(drawingCount);
 
     // build the array of squares one row at a time
     for (int j = 0; j < 22; j++) { // Columns
@@ -53,81 +61,74 @@ void testApp::buildSquares(){
             
             squares.push_back(s);
         }
-    }    
+    }
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
-
-//*  // only update the screen up to 1024 frames
-    if (ofGetFrameNum() <= 1023) {
-        
-        // buildSquares vector
-        buildSquares();
-        
-        // draw the pre-built array of squares one row at a time
-        for (int i = 0; i < squares.size(); i++) {
-            int w = 16; //size of squares
-            int h = w;
             
-            ofSetColor(squares[i].r, squares[i].g, squares[i].b);
-            
-            // position and draw the squares themselves
-            ofPushMatrix();         // push the current coordinate position
-            ofTranslate(squares[i].x, squares[i].y, 0);   // move the current coordinate system to (x,y) and make that zero
-            ofPushMatrix();         // push the current coordinate position
-            ofRotateZ(squares[i].rZ);           // rotate the coordinate system
-            ofRect(0, 0, w, h);     // draw a rect at (0,0)
-            ofPopMatrix();          // recall the pushed coordinate position
-            ofPopMatrix();          // recall the pushed coordinate position        
-        }
+    // draw the pre-built array of squares one row at a time
+    for (int i = 0; i < squares.size(); i++) {
+        int w = 16; //size of squares
+        int h = w;
         
-        // print a title
-        int w = 16;
-        int h = w;   
-        ofSetColor(ofColor::black);         // use black as text color
-        ofTrueTypeFont  myFont;
-        myFont.loadFont("verdana.ttf", w/2, true, true);
-        myFont.setLineHeight(18.0f);
-        myFont.setLetterSpacing(1.037);
-        string title = "da_2013(\"Schotter 1968\") | f.";
-        title.append(ofToString(ofGetFrameNum())); 
-        //title.append(" / °");
-        myFont.drawString(title, w * 14, h * 26.5);
-        //    ofSetWindowTitle(title);
-
-        // Handle framerates and pausing at beginning and end
-        if (ofGetFrameNum() == 0) {
-        } else if (ofGetFrameNum() == 1) {
-            // pause for 5 seconds on the first frame
-            while (ofGetElapsedTimeMillis() <5000){
-                // print something to let me know I'm in a loop
-            }
-            
-            // ofSleepMillis(5000);
-        } else if (ofGetFrameNum() > 1 && ofGetFrameNum() < 1023 ) {
-            // set the frame rate to increase as frame count approaches 512
-            // then decrease as frane count approaches 1024
-            int r = 512 - abs(512-ofGetFrameNum());
-            ofSetFrameRate(r);
-        } else if (ofGetFrameNum() == 1023) {
-            // disable clearing the frame on the next frame
-            ofSetBackgroundAuto(false);
-        } 
-        ofResetElapsedTimeCounter();
+        ofSetColor(squares[i].r, squares[i].g, squares[i].b);
+        
+        // position and draw the squares themselves
+        ofPushMatrix();         // push the current coordinate position
+        ofTranslate(squares[i].x, squares[i].y, 0);   // move the current coordinate system to (x,y) and make that zero
+        ofPushMatrix();         // push the current coordinate position
+        ofRotateZ(squares[i].rZ);           // rotate the coordinate system
+        ofRect(0, 0, w, h);     // draw a rect at (0,0)
+        ofPopMatrix();          // recall the pushed coordinate position
+        ofPopMatrix();          // recall the pushed coordinate position        
     }
+    
+    // print a title
+    int w = 16;
+    int h = w;   
+    ofSetColor(ofColor::black);         // use black as text color
+    ofTrueTypeFont  myFont;
+    myFont.loadFont("verdana.ttf", w/2, true, true);
+    myFont.setLineHeight(18.0f);
+    myFont.setLetterSpacing(1.037);
+    string title = "da_2013(\"Schotter 1968\") | d.";
+    title.append(ofToString(drawingCount)); 
+    //title.append(" / °");
+    myFont.drawString(title, w * 14, h * 26.5);
+    //    ofSetWindowTitle(title);
+    
+    // update drawing if it has been displayed long enough
+    if (ofGetElapsedTimeMillis() > waitMillis){
+            updateDrawing();
+    }
+    
 //*/
+}
+
+void testApp::updateDrawing(){
+    
+    ofResetElapsedTimeCounter();
+    drawingCount++;
+
+    if (drawingCount) {
+        // update waitMillis so drawings remain on screen for a second at first, speeding up then slowing down again anytime the drawingCount approaches a multiple of waitMod 
+        int halfMod = waitMod/2;
+        waitMillis = 1000 / (1+(2 * (halfMod - abs(halfMod - (drawingCount % waitMod)))));
+    } else {
+        // do nothing, waitMillis will remain the value it was initialized at in ini() -- 5000
+    }
+
+    cout << "waitMillis = " << waitMillis << endl;
+    
+    buildSquares();
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
     if (key == 's') {
         ofSaveScreen("test_"+ofToString(ofGetFrameNum())+".png");
-    }
-    if (key == 'b') {
-        buildSquares();
-    }
-        
+    }        
 }
 
 //--------------------------------------------------------------
@@ -147,7 +148,7 @@ void testApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
-    buildSquares();
+
 }
 
 //--------------------------------------------------------------
--- src/testApp.h
+++ src/testApp.h
@@ -4,32 +4,36 @@
 
 class mySquare {
 public:
-    int x,y,rZ, r,g,b;
+    int     x,y,rZ, r,g,b;
     
 };
 
 class testApp : public ofBaseApp{    
 public:
-    void setup();
-    void update();
-    void draw();
+    void    setup();
+    void    update();
+    void    draw();
     
-    void keyPressed(int key);
-    void keyReleased(int key);
-    void mouseMoved(int x, int y);
-    void mouseDragged(int x, int y, int button);
-    void mousePressed(int x, int y, int button);
-    void mouseReleased(int x, int y, int button);
-    void windowResized(int w, int h);
-    void dragEvent(ofDragInfo dragInfo);
-    void gotMessage(ofMessage msg);
+    void    keyPressed(int key);
+    void    keyReleased(int key);
+    void    mouseMoved(int x, int y);
+    void    mouseDragged(int x, int y, int button);
+    void    mousePressed(int x, int y, int button);
+    void    mouseReleased(int x, int y, int button);
+    void    windowResized(int w, int h);
+    void    dragEvent(ofDragInfo dragInfo);
+    void    gotMessage(ofMessage msg);
     
-    void buildSquares();
+    void    buildSquares();
+    void    updateDrawing();
     
-    vector <mySquare> squares;
-        
-    int w;
-    int h;
+    int     drawingCount;
+    vector  <mySquare> squares;
+    int     waitMillis;
+    int     waitMod;
+
+    int     w;
+    int     h;
 
     ofTrueTypeFont  myFont;
 };
